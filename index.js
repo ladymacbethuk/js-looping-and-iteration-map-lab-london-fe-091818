42424e544cd418579ad759d146bb57efb9d2ddfe
Stephanie Phillips <steph@ladymacbeth.uk>
2018-11-04 20:40:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
-// Code your solution in this file.
+
+function lowerCaseDrivers(drivers) {
+  return drivers.map(d=>d.toLowerCase());
+}
+
+function nameToAttributes(drivers) {
+  return drivers.map(x=> {
+    let z= x.split(" ");
+    return {
+      firstName : z[0],
+      lastName : z[1]
+    };
+  })
+
+}
+
+
+function attributesToPhrase(d) {
+  return d.map(function(x) {
+    return x.name + " is from " + x.hometown;
+});
+}
+
+/*
+//add 2
+function add(x,y){
+  return x+y;
+}
+
+//subtract 2
+function sub(x,y){
+  return x-y;
+}
+
+//multiply 2
+function mult(x,y) {
+  return x*y;
+}
+
+let names = ["Steph Phillips", "Bob Marley", "Anita Livingston"];
+
+//splitstr
+function splitstr(names) {
+    let x= names.split(" ");
+    return {
+      firstName : x[0],
+      lastName : x[1]
+    }; 
+}
+
+
+
+//joinstr
+function myFunction() {
+    let choc = ["Mars", "Twix", "Bounty"];
+    return choc.join();
+}
+
+
+//hello
+function hello(name){
+  return "Hello "+ name + " !";
+}
+*/
